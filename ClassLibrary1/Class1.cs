a3ec8e163550a3a9963a4f6fa132a5e5df3d2dca
Vicki Shaw <vickishaw94@gmail.com>
2017-05-23 15:58:12 -0400

    working basic class to write cwd

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        public static void Main()
+        {
+            string[] lines = { Directory.GetCurrentDirectory()};
+            string place =  "C:\\Users\\vs\\Documents\\systemLogs\\";
+
+            //string place = Directory.GetCurrentDirectory();
+            System.IO.File.WriteAllLines(place + "writeLines.txt", lines);
+
+        }
+    }
+}
