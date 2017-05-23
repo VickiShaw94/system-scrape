a473bef4c73264a495f63feb4299d00799987866
Vicki Shaw <vickishaw94@gmail.com>
2017-05-23 16:38:01 -0400

    added process to execute msinfo.exe

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,20 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Diagnostics;
 
 namespace ClassLibrary1
 {
+
     public class Class1
     {
+
+        public static void collectSysInfo()
+        {
+        }
 
         public static void Main()
         {
             string[] lines = { Directory.GetCurrentDirectory()};
             string place =  "C:\\Users\\vs\\Documents\\systemLogs\\";
 
+            Process sysInfoCaptureExternalProcess = new Process();
+            sysInfoCaptureExternalProcess.StartInfo.UseShellExecute = false;
+            sysInfoCaptureExternalProcess.StartInfo.FileName = "msinfo32.exe";
+            sysInfoCaptureExternalProcess.Start();
             //string place = Directory.GetCurrentDirectory();
             System.IO.File.WriteAllLines(place + "writeLines.txt", lines);
 
         }
+
+        
     }
 }
