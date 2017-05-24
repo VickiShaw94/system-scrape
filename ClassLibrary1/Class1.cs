ffd6ecfb0d7752c64af514edaa90a57d12c87c0e
Vicki Shaw <vickishaw94@gmail.com>
2017-05-24 12:10:01 -0400

    saves msinfo32 to text file

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -14,38 +14,23 @@ namespace ClassLibrary1
     }
     public class Class1
     {
-        //instantiating class process
-        public static Process externalProcess = new Process();
- 
-        private static void setFileName(string fileName)
-        {
-            externalProcess.StartInfo.FileName = fileName;
-        }               
-
-        /**
-         * Sets Arguments -- string containing the arguments to pass to the target app
-         * specified in setFileName property, default empty string ""
-         *  
-        */
-
-        private static void setSaveLocation(string loc)
-        {
-            externalProcess.StartInfo.WorkingDirectory = loc;
-        }
-
+        
         public static void Main()
         {
+            Process externalProcess = new Process();
 
-            setFileName("msinfo32.exe");
-            //setArguments("/report" + zzz + ".txt");
-            externalProcess.StartInfo.Arguments = "/report zzz.txt";
+            externalProcess.StartInfo.FileName = "msinfo32.exe";
+            string saveLocation =  "C:\\Users\\vs\\Documents\\systemLogs\\";
+            externalProcess.StartInfo.Arguments = "/report C:\\Users\vs\\Documents\\systemLogs\\TEST.txt";
 
-            setSaveLocation(Directory.GetCurrentDirectory);
+                // + saveLocation + "text.NFO";
+
+            externalProcess.StartInfo.UseShellExecute = false;
+            externalProcess.StartInfo.WorkingDirectory = saveLocation;
             externalProcess.Start();
 
 
             /*
-            string place =  "C:\\Users\\vs\\Documents\\systemLogs\\";
 
             sysInfoCaptureExternalProcess.StartInfo.UseShellExecute = false;
             sysInfoCaptureExternalProcess.StartInfo.FileName = "msinfo32.exe";
