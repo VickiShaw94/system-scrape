c354bc705a93ed36aefeb06d86b7c6ef9c76e36c
Vicki Shaw <vickishaw94@gmail.com>
2017-05-24 14:33:44 -0400

    saves system info text file to root of executable

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -5,44 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ClassLibrary1
 {
 
-    public class Class2 { 
-
-    }
     public class Class1
     {
-        
         public static void Main()
         {
             Process externalProcess = new Process();
-
             externalProcess.StartInfo.FileName = "msinfo32.exe";
-            string saveLocation =  "C:\\Users\\vs\\Documents\\systemLogs\\";
-            externalProcess.StartInfo.Arguments = "/report C:\\Users\vs\\Documents\\systemLogs\\TEST.txt";
-
-                // + saveLocation + "text.NFO";
-
-            externalProcess.StartInfo.UseShellExecute = false;
-            externalProcess.StartInfo.WorkingDirectory = saveLocation;
+            externalProcess.StartInfo.Arguments = "/report " + @".\sysInfo.txt";
             externalProcess.Start();
-
-
-            /*
-
-            sysInfoCaptureExternalProcess.StartInfo.UseShellExecute = false;
-            sysInfoCaptureExternalProcess.StartInfo.FileName = "msinfo32.exe";
-            sysInfoCaptureExternalProcess.StartInfo.Arguments = "/report.txt";
-            sysInfoCaptureExternalProcess.StartInfo.WorkingDirectory = place;
-            sysInfoCaptureExternalProcess.Start();
-            //string place = Directory.GetCurrentDirectory();
-            System.IO.File.WriteAllLines(place + "writeLines.txt", lines);
-
-             */   
         }
-
-        
     }
 }
