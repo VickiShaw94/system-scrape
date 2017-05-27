7b75406ca0e209ab53e69b88e326276b98526084
Vicki Shaw <vickishaw94@gmail.com>
2017-05-27 14:21:55 -0400

    added startup programs to txt file

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Management;
@@ -29,7 +30,7 @@ namespace ClassLibrary1
         /// <param name="path"> specify which event log</param>
         /// <param name="query"> what type of events in the log </param>
         /// <param name="targetPath"> where do you want to save it to </param>
-        /// <param name="flags"> default specified by enum EventExportLogFlags</param>
+        /// <param name="flags"> default spe</param>
         /// <returns></returns>
         [DllImport(@"wevtapi.dll",
             CallingConvention = CallingConvention.Winapi,
@@ -41,8 +42,6 @@ namespace ClassLibrary1
             string query,
             string targetPath,
             [MarshalAs(UnmanagedType.I4)] EventExportLogFlags flags);
-
-
 
         /// <summary>
         /// Method returns machine serial number in string
@@ -70,24 +69,31 @@ namespace ClassLibrary1
         /// Executes external process to call MsInfo32.exe, saves the information to
         /// txt file with serial number name
         /// </summary>
-
         private static void extractMsInfo32()
         {
             Process extProcess = new Process();
-            string serialID = retrieveSerial();
             extProcess.StartInfo.FileName = "msinfo32.exe";
-            extProcess.StartInfo.Arguments = "/report " + @".\" + serialID + ".txt";
+            extProcess.StartInfo.Arguments = "/report " + @".\" + retrieveSerial() + ".txt";
             extProcess.Start();
         }
 
+        /// <summary>
+        /// method gets startup programs to text file
+        /// </summary>
+        private static void getStartups()
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddScript("wmic startup > " + @".\" + retrieveSerial() + "_startup.txt");
+                ps.Invoke();
+            }
 
+        }
         public static void Main()
         {
-
             EvtExportLog(IntPtr.Zero, "System", "*", @".\sys.evtx", EventExportLogFlags.ChannelPath);
-            //getEventLog("SYSTEMEVENTLOG");
-            //writeWinEventLog("try1.txt");
-            //extractMsInfo32();
+            extractMsInfo32();
+            getStartups();
         }
     }
 }
